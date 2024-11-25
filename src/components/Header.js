78a612d36ae9570ee4a5ac3375f48bf11e2ff91b
Ehsan <ctech1699@gmail.com>
2024-11-26 03:33:13 +0530

    navbar Chagne test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import DesignSessionForm from "./DesignSessionForm";
 import Link from "next/link";
@@ -7,13 +7,37 @@ import Link from "next/link";
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [openForm, setOpenForm] = useState(false);
+  const [showHeader, setShowHeader] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
   // Toggle the mobile menu
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
+  // Handle scroll behavior
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY && currentScrollY > 100) {
+        setShowHeader(false); // Hide header on scroll up
+      } else {
+        setShowHeader(true); // Show header on scroll down
+      }
+      setLastScrollY(currentScrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <div>
-      <header className="sticky top-0 z-50 bg-white shadow-md">
+      <header
+        className={`fixed top-0 z-50 w-full transition-transform duration-300 ${
+          showHeader ? "translate-y-0" : "-translate-y-full"
+        } bg-background shadow-md`}
+      >
         <div className="container mx-auto flex justify-between items-center py-4 px-6">
           {/* Logo and Title */}
           <Link href="/" className="flex items-center space-x-3">
@@ -21,7 +45,7 @@ const Header = () => {
             <img
               src="/logo2nd.png"
               alt="The Interiux"
-              className="h-10 object-contain" // Ensures it doesn't stretch and stays within the proper height
+              className="h-10 object-contain"
             />
             <h1 className="text-2xl font-bold text-primary">The Interiux</h1>
           </Link>
@@ -30,31 +54,31 @@ const Header = () => {
           <nav className="hidden md:flex space-x-8 items-center">
             <Link
               href="/"
-              className="text-gray-800 hover:text-primary transition duration-300"
+              className="text-secondary hover:text-primary transition duration-300"
             >
               Home
             </Link>
             <a
               href="/about"
-              className="text-gray-800 hover:text-primary transition duration-300"
+              className="text-secondary hover:text-primary transition duration-300"
             >
               About Us
             </a>
             <a
               href="#services"
-              className="text-gray-800 hover:text-primary transition duration-300"
+              className="text-secondary hover:text-primary transition duration-300"
             >
               Services
             </a>
             <a
-              href="#gallary"
-              className="text-gray-800 hover:text-primary transition duration-300"
+              href="#gallery"
+              className="text-secondary hover:text-primary transition duration-300"
             >
               Gallery
             </a>
             <a
               href="#contact"
-              className="text-gray-800 hover:text-primary transition duration-300"
+              className="text-secondary hover:text-primary transition duration-300"
             >
               Contact Us
             </a>
@@ -72,7 +96,7 @@ const Header = () => {
           {/* Book Consultation Button */}
           <button
             onClick={() => setOpenForm(true)}
-            className="hidden md:block bg-primary text-white py-2 px-6 rounded-lg shadow-md hover:bg-focus transition duration-300"
+            className="hidden md:block bg-primary text-secondary py-2 px-6 rounded-lg shadow-md hover:bg-focus transition duration-300"
           >
             Book Consultation
           </button>
@@ -84,31 +108,31 @@ const Header = () => {
             <nav className="space-y-4 text-center">
               <a
                 href="#home"
-                className="block text-white hover:text-gray-800 transition duration-300"
+                className="block text-secondary hover:text-background transition duration-300"
               >
                 Home
               </a>
               <a
                 href="/about"
-                className="block text-white hover:text-gray-800 transition duration-300"
+                className="block text-secondary hover:text-background transition duration-300"
               >
                 About Us
               </a>
               <a
                 href="#services"
-                className="block text-white hover:text-gray-800 transition duration-300"
+                className="block text-secondary hover:text-background transition duration-300"
               >
                 Services
               </a>
               <a
-                href="#gallary"
-                className="block text-white hover:text-gray-800 transition duration-300"
+                href="#gallery"
+                className="block text-secondary hover:text-background transition duration-300"
               >
                 Gallery
               </a>
               <a
                 href="#contact"
-                className="block text-white hover:text-gray-800 transition duration-300"
+                className="block text-secondary hover:text-background transition duration-300"
               >
                 Contact Us
               </a>
@@ -117,7 +141,7 @@ const Header = () => {
                   setOpenForm(true);
                   setIsMenuOpen(false);
                 }}
-                className="block w-full bg-white text-primary py-2 px-4 rounded-lg mt-4 hover:bg-yellow-500 transition duration-300"
+                className="block w-full bg-secondary text-primary py-2 px-4 rounded-lg mt-4 hover:bg-focus transition duration-300"
               >
                 Book Consultation
               </button>
@@ -128,20 +152,20 @@ const Header = () => {
 
       {/* Show the Design Session Form when the state is true */}
       {openForm && <DesignSessionForm setOpenForm={setOpenForm} />}
-      {/* Whatsapp icon */}
+
+      {/* WhatsApp Icon */}
       <a
         href="https://wa.me/919022485604"
-        className="fixed bottom-4 right-0 z-50"
+        className="fixed bottom-4 right-4 z-50"
         target="_blank"
         rel="noreferrer"
-        >
+      >
         <img
           src="/whatsappicon.png"
-          alt="Whatsapp Icon"
+          alt="WhatsApp Icon"
           className="h-16 object-contain"
         />
-        </a>
-        
+      </a>
     </div>
   );
 };
